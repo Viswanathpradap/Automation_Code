85509d05a57cc4721d40b0e1b769970b4972be00
Viswanathpradap <161550369+Viswanathpradap@users.noreply.github.com>
2024-02-29 10:07:47 +0530

    free job alert text search

--- First_demo.java
+++ First_demo.java
@@ -1,5 +1,7 @@
 package viswa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,10 +14,14 @@ public class First_demo {
             
    WebDriver driver=new ChromeDriver();  
      driver.manage().window().maximize();  
-     driver.get("https://www.leafground.com/dashboard.xhtml");
+     driver.get("https://www.freejobalert.com/");
      
-     driver.close();
+     driver.findElement(By.xpath("//*[@id=\"gsc-i-id1\"]")).sendKeys("state bank"+Keys.ENTER);
      
+     driver.findElement(By.xpath("//*[@id=\"___gcse_0\"]/div/div/div/div[5]/div[2]/div/div/div[1]/div[1]/div/div[1]/div/a")).click();
+     
+    //driver.close();
+   
      
 	     
 
